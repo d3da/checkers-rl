976b8a755355d5dfa2722f2fd0d86a86cb8a3004
d3da <daandieperink@gmail.com>
2024-01-07 17:59:12 +0100

    Fix dependency installation for jupyterlab

--- install_requirements.sh
+++ install_requirements.sh
@@ -3,8 +3,8 @@ set -eux
 
 pip install -U pip tqdm numpy pydraughts pandas matplotlib scikit-learn scikit-optimize
 
-if [[ "$USER" = "joyvan" ]]; then
-    pip install -U torch~=2.1.1+cu116 --extra-index-url https://download.pytorch.org/whl/cu116
+if [[ "$USER" = "jovyan" ]]; then
+    pip install -U torch==1.13.1+cu116 --extra-index-url https://download.pytorch.org/whl/cu116
 else
     pip install -U torch
 fi
