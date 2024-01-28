646bd72a25fc3e5ec1762c1cfbbace1e03540b35
d3da <daandieperink@gmail.com>
2024-01-28 20:08:41 +0100

    Add pyarrow as dependency

--- install_requirements.sh
+++ install_requirements.sh
@@ -1,7 +1,7 @@
 #/bin/bash
 set -eux
 
-pip install -U pip tqdm numpy pydraughts pandas matplotlib scikit-learn scikit-optimize
+pip install -U pip tqdm numpy pydraughts pandas matplotlib scikit-learn scikit-optimize pyarrow
 
 if [[ "$USER" = "jovyan" ]]; then
     pip install -U torch==1.13.1+cu116 --extra-index-url https://download.pytorch.org/whl/cu116
